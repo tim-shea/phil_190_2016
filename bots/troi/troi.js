103b2ccb92e1cb7b2143f7ed53022bf92923fdf5
Khrystarlite <ctaiine.rasengai@gmail.com>
2016-02-02 12:37:35 -0800

    angle>> speed

--- bots/troi/troi.js
+++ bots/troi/troi.js
@@ -5,12 +5,12 @@ troi.speed = 100;
 troi.update = function() {
     if (Math.random() < .1) {
         troi.angle += 5;
-    }
+    }speed
     if (Math.random() < 0.1) {
     	if (Math.random() < 0.05) {
-    		troi.angle -= 10;
+    		troi.speed = 40;
     	}else {
-    		troi.angle += .5;
+    		troi.speed = 120;
     	}
     };
 };
