40b00727766fc661db7d2abd77c7b194aec1234f
Insectoid <arthropodlover@gmail.com>
2016-02-02 13:03:29 -0800

    sharAI talks!
    Now they can say "Hello!" back to you! Granted, their range of
    conversation is limited at the moment, but it can be expanded at anytime
    fairly easily. Anyone is free to grab the talk() function and the
    related strings and paste it into their bot.

--- bots/sharAI/sharAI.js
+++ bots/sharAI/sharAI.js
@@ -1,16 +1,37 @@
 var sharAI = new Bot(100, 100, 'sharAI', 'bots/sharAI/sharAI.png');
 sharAI.angle = 90;
 sharAI.speed = 100;
+sharAI.stateText = "sharAI says: Hello!";
 
 var turnRandom = 0;
 
-sharAI.update = function() {
-	turnRandom = Math.random();
-    if (turnRandom < .1) {
-        sharAI.angle += 5;
-        //console.log("sharAI is turning a little bit to the left");
-    } else if (turnRandom >= .1 && turnRandom < .2) {
-    	sharAI.angle -= 5;
-    	//console.log("sharAI is turning a little bit to the right");
-    }
+sharAI.talk = function(array) {
+    return array[Math.floor(Math.random() * array.length)]
 };
+
+var likeStrings = ["video games", "my mom"];
+var talkStrings = ["Beep boop!", ("I miss " + sharAI.talk(likeStrings) )];
+
+sharAI.getStatus = function() {
+    return sharAI.stateText;
+}
+
+sharAI.update = function() {
+    turnRandom = Math.random();
+    if (turnRandom < .1) {
+        sharAI.speeed = 100;
+        sharAI.angle += 5;
+    } else if (turnRandom >= .1 && turnRandom < .2) {
+        sharAI.speed = 100;
+        sharAI.angle -= 5;
+    }
+
+    if (turnRandom < .01) {
+        sharAI.speed = 0;
+        if (Math.random() < .1) {
+            sharAI.stateText = ("sharAI says: " + sharAI.talk(talkStrings) );
+        }
+    }
+}
+
+
