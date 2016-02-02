789605676f67b60e2334871e36ebf3f2dc19f707
Khrystarlite <ctaiine.rasengai@gmail.com>
2016-02-02 12:59:28 -0800

    temp_0

--- bots/troi/troi.js
+++ bots/troi/troi.js
@@ -1,16 +1,18 @@
-var troi = new Bot(120, 120, 'troi', 'bots/troi/umbreon.gif');
+var troi = new Bot(30, 30, 'troi', 'bots/troi/umbreon.gif');
 troi.angle = 50;
 troi.speed = 100;
 
 troi.update = function() {
     if (Math.random() < .1) {
-        troi.angle += 5;
-    }speed
+        troi.angle += .5;
+    }else{
+    	troi.angle -= 2;
+    }
     if (Math.random() < 0.1) {
     	if (Math.random() < 0.05) {
-    		troi.speed = 40;
+    		troi.speed = 10;
     	}else {
-    		troi.speed = 120;
+    		troi.speed = 100;
     	}
     };
 };
