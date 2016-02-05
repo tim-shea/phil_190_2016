da4b2afd754561d40071e9721bee474959e9b775
Insectoid <arthropodlover@gmail.com>
2016-02-04 23:44:21 -0800

    sharAI gets an upgrade
    - New sprite!
    - sharAI now needs to take naps!
    - sharAI can now say more stuff
    - Status updates

--- bots/sharAI/sharAI.js
+++ bots/sharAI/sharAI.js
@@ -1,36 +1,135 @@
+//
+// INIT
+//
 var sharAI = new Bot(100, 100, 'sharAI', 'bots/sharAI/sharAI.png');
 sharAI.angle = 90;
 sharAI.speed = 100;
-sharAI.stateText = "sharAI says: Hello!";
+sharAI.state = 1;
+sharAI.stateText = "SYSTEMS NORMAL \nsharAI says: Hello!";
+sharAI.sleepDrive = 0;
+sharAI.anger = 0;
 
 var turnRandom = 0;
+var currentStatus = "SYSTEMS NORMAL \n";
+var currentDiscussion = "Hello!";
+var walkSprite = ['bots/sharAI/sharAI.png', 'bots/sharAI/sharAI WALK 1.png'];
+var walkIndex = 0;
+
+//
+// MOVEMENT
+//
+
+sharAI.move = function() {
+    turnRandom = Math.random();
+    if (turnRandom < .1) {
+        sharAI.angle += 5;
+    } else if (turnRandom >= .1 && turnRandom < .2) {
+        sharAI.angle -= 5;
+    }
+
+    // Sprite cycling
+    walkIndex++;
+    if (walkIndex % walkSprite.length == 0) {
+        walkIndex = 0;
+    }
+    sharAI.imagePath = walkSprite[walkIndex];
+
+}
+
+//
+// TALK INIT & SCRIPTS
+//
+
+sharAI.say = function() {
+    if (sharAI.sleepDrive > 900) {
+        currentDiscussion = "I'm getting sleepy...";
+    } 
+    else if (Math.random() < .01) {
+        currentDiscussion = sharAI.talk(talkStrings);
+    }
+}
 
 sharAI.talk = function(array) {
     var string = array[Math.floor(Math.random() * array.length)];
     return string;
 };
 
-var likeStrings = ["video games", "my mom"];
-var talkStrings = ["Beep boop!", ("I miss " + sharAI.talk(likeStrings))];
+var stateStrings = ["STATE ERROR FOUND. PLEASE DEBUG! ", "sharAI is doing ok.", "sharAI is taking a nap."];
+var identityStrings = ["a robot", "a bot", "a mind \(maybe?\)", "a person \(possibly?\)", "neither a man nor a woman", "nihilistic"];
+var likeStrings = ["video games", "my family", "my friends", "bugs", "slugs", "snails", "monsters", "robots", "computers", "programming", "sleeping", "food", "ingesting fluids", "swearing"];
+var cursePrefixes = ["God ", "Jesus ", "Ass", "Shit", "Fuck", "Son of a ", "Mother", "Father", "Goat", "Butt", "Anus", "Monkey", "Bull", "Bumble", "Cock", "Dick", "Weasel", "Horse", "Sweet", "Baby", "Christ ", "Holy", "Bitch", "Bastard", "Dip", "Douche", "Dumb", "Fucker", "Suck", "Fudge", "Hacker ", "Jack", "Jerk", "Lame", "Camel", "Piss", "Prick", "Schlong", "Scrote", "Shitty ", "Uncle", "Aunt", "Boob", "Nerd", "Noob", "Beard", "Cauldron", "Banshee", "Dragon", "Rabble", "Broom", "Taint", "Turd", "Unicorn", "Sock", "Nipple", "Goblin", "Crotch", "\*\@\$\#"];
+var curseSuffixes = ["er", "damn", " Christ", "fucker", "hole", "ass", "goat", "gun", "shit", "anus", "hat", "pirate", "bag", "clown", "butt", "face", "goblin", "muncher", "head", "licker", "monkey", "wad", "wipe", "boner", "poop", "cock", "booty", "burger", "master", "nugget", "smith", "waffle", "bubble", "monger", "mongrel", "dick", "weasel", "dong", "dildo", "nerd", "weed", " on a pogo", " on a bike", " on a cracker", " Jesus", "baby", "bitch", "bastard", "tickler", "whisperer", "douche", "butter", "boy", "man", "sucker", "nut", "up", "wit", "fudge", "camel", "piss", "prick", "schlong", "scrote", "shitter", "uncle", "aunt", "vagina", "boob", "wanker", "noob", "beard", "bum", "banshee", "dragon", "bougies", "rouser", "broom", "crack", "taint", "turd", "unicorn", "polisher", "sock", "nipple", "crotch", "\*\@\$\#"]; // Wow, I didn't think you'd read this line all the way through! Congratulations!
+var curseStrings = ["Fuck!", "Shit!", "Ass!", "Damn!", "Hell!", "Butt!", "Dick!", "Bastard!", "Fudge!", "Piss!", "Dingleberry!", "\*\@\$\#!", "Gadzooks!", (sharAI.talk(cursePrefixes) + sharAI.talk(curseSuffixes) + "!")];
+var factStrings = [("I am " + sharAI.talk(identityStrings) + "?"), "snails. talk. really. slowly?", ("I like " + sharAI.talk(likeStrings) + "?"), "nothing matters?"];
+var talkStrings = ["Beep boop!", ("Did you know that " + sharAI.talk(factStrings)), ("I like " + sharAI.talk(likeStrings) + "!")];
 
+
+//
+// MAIN
+//
 sharAI.getStatus = function() {
+    currentStatus = ("" + stateStrings[sharAI.state] + "\n");
+    sharAI.stateText = ("" + currentStatus + "sharAI says: " + currentDiscussion);
     return sharAI.stateText;
 }
 
 sharAI.update = function() {
-    turnRandom = Math.random();
-    if (turnRandom < .1) {
-        sharAI.speeed = 100;
-        sharAI.angle += 5;
-    } else if (turnRandom >= .1 && turnRandom < .2) {
-        sharAI.speed = 100;
-        sharAI.angle -= 5;
+    //
+    // STATUS HANDLER
+    //
+    switch (sharAI.state) {
+        case 1: // NORMAL STATUS
+            sharAI.speed = 100;
+            sharAI.move();
+            sharAI.say()
+            sharAI.sleepDrive++;
+            break;
+
+        case 2: // NAP TIME
+            sharAI.speed = 0;
+            sharAI.sleepDrive--;
+
+            // Makes sharAI snore, but not too much
+            if (sharAI.sleepDrive % 10 == 0) {
+                currentDiscussion = (currentDiscussion + "z");
+            }
+
+            // Wake sharAI up once they're not tired anymore
+            if (sharAI.sleepDrive == 0) {
+                sharAI.state = 1;
+                currentDiscussion = "Yawn...";
+            }
+            break;
+
+        // TODO: Make case 3 & 4 for dealing with hunger and thirst
+
+        case 5: // RAGE MODE
+            sharAI.speed = 0;
+            sharAI.angle += 15; // Tantrum spin!
+            currentDiscussion = (currentDiscussion + sharAI.talk(curseStrings) + " "); // Blow some steam!
+            sharAI.anger--;
+
+            // Return sharAI to normal once they've cursed their anger away
+            if (sharAI.anger == 0) {
+                sharAI.state = 1;
+            }
+            break;
+
+        default: // ERROR HANDLER
+            sharAI.state = 0;
+            sharAI.speed = 0;
+            break;
     }
 
-    if (turnRandom < .01) {
-        sharAI.speed = 0;
-        if (Math.random() < .5) {
-            sharAI.stateText = ("sharAI says: " + sharAI.talk(talkStrings));
-        }
+    //
+    // STATUS SWITCHER
+    //
+    if (sharAI.sleepDrive >= 1000) { //If sharAI gets too tired, put them into sleep mode
+        sharAI.state = 2
+        currentDiscussion = "Z";
+    }
+    else if (sharAI.anger >= 10) { //If sharAI gets too angry, let them blow off some steam
+        sharAI.state = 5
+        currentDiscussion = "";
     }
 }
