6b2018b3ee6318a7c805e3835cc9eb0f210aee18
duyenny <duyen.tran@hotmail.com>
2016-02-08 23:55:07 -0800

    fixing errors

--- bots/duyen/duyen.js
+++ bots/duyen/duyen.js
@@ -5,22 +5,22 @@ duyen.speed = 100;
 
 duyen.stateText = "just flowing around";
 
-// duyen.getStatus = function();
-// 	return duyen.stateText;
-// }
+duyen.getStatus = function() {
+ 	return duyen.stateText;
+}
 
-// duyen.update = function(); {
-// 	if (Math.random() < .1) {
-// 		duyen.angle +=5;
-// 		duyen.angle = duyen.angle % 120; 
-// 	}
+duyen.update = function() {
+ 	if (Math.random() < .1) {
+ 		duyen.angle +=5;
+ 		duyen.angle = duyen.angle % 120; 
+ }
 	
-// 	if (Math.random() < 0.1) {
-// 		if (Math.random() < 0.05) {
-// 			duyen.speed = 10;
-// 		}
-// 		else {
-// 			duyen.speed = 100;
-// 		}
-// 	}
-// };
+if (Math.random() < 0.1) {
+	if (Math.random() < 0.05) {
+			duyen.speed = 10;
+ 		}
+ 		else {
+ 			duyen.speed = 100;
+ 		}
+ 	}
+ };
