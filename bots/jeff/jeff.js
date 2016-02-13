edfef15ed1763be3795c7f1aca1ba5f3bae879ee
Jeff Yoshimi <jyoshimi@ucmerced.edu>
2016-02-12 16:20:32 -0800

    Stub to show to handle timed events

--- bots/jeff/jeff.js
+++ bots/jeff/jeff.js
@@ -12,6 +12,15 @@ jeff.init = function() {
     this.body = this.sprite.body;  // Todo:  a way to automate this?
     jeff.body.rotation = 100; // Initial Angle
     jeff.body.speed = 100; // Initial Speed
+    // Do something every n seconds.
+    // game.time.events.loop(Phaser.Timer.SECOND * 1, jeff.timedEvend, this);
+}
+
+//
+// Example of a timed event
+//
+jeff.timedEvend = function() {
+    console.log(game.time.totalElapsedSeconds());   
 }
 
 jeff.getStatus = function() {
