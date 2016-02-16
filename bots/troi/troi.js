eab9401d55b4b2188faa0bfa010d37e4c3778128
Khrystarlite <ctaiine.rasengai@gmail.com>
2016-02-16 12:24:56 -0800

    test_0 Signed-off-by: Khrystarlite <ctaiine.rasengai@gmail.com>

--- bots/troi/troi.js
+++ bots/troi/troi.js
@@ -1,18 +1,18 @@
 var troi = new Bot(250, 250, 'troi', 'bots/troi/umbreon_2.0.png');
-troi.angle = 50;
-troi.speed = 100;
+troi.angle.speed = 50;
+troi.body.speed = 100;
 
 troi.update = function() {
     if (Math.random() < .1) {
-        troi.angle += .5;
+        troi.angle.speed += .5;
     }else{
-    	troi.angle -= 2;
+    	troi.angle.speed -= 2;
     }
     if (Math.random() < 0.1) {
     	if (Math.random() < 0.05) {
-    		troi.speed = 10;
+    		troi.body.speed = 10;
     	}else {
-    		troi.speed = 100;
+    		troi.body.speed = 100;
     	}
     };
 };
