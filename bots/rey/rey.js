06c86289acbd780e771a938288dd734ed334c0ac
reyflores94 <rsequeira@ucmerced.edu>
2016-02-16 12:52:18 -0800

    Fixed Bot
    Bot

--- bots/rey/rey.js
+++ bots/rey/rey.js
@@ -1,8 +1,20 @@
 var rey = new Bot(240, 220, 'rey', 'bots/rey/whitedeer.png');
-rey.angle = 50; // Initial Angle
-rey.speed = 100; // Initial Speed
+// rey.body.angle = 100; // Initial Angle
+// rey.body.speed = 100; // Initial Speed
+// rey.body.rotation = 100; 
+
 
 rey.stateText = "Beware the break-dancing mighty deer!"; //break-dancing mighty deer because everytime it stops to rest it moves like crazy
+
+rey.init = function() {
+    this.body = this.sprite.body;  // Todo:  a way to automate this?
+    rey.body.rotation = 100; // Initial Angle
+    rey.body.speed = 100; // Initial Speed
+    // Do something every n seconds.
+    // game.time.events.loop(Phaser.Timer.SECOND * 1, rey.timedEvend, this);
+}
+
+
 
 rey.getStatus = function() {
     return rey.stateText;
@@ -10,16 +22,18 @@ rey.getStatus = function() {
 
 rey.update = function() {
     if (Math.random() < .1) {
-        rey.angle += 100;
-        rey.angle = rey.angle % 180;
+        //rey.angle += 100;
+        rey.incrementAngle(100);
     }
     if (Math.random() < .01) {
         if (Math.random() < .5) {
-            rey.speed = 300;
+            rey.body.speed = 300;
             rey.stateText = "Here comes the break-dancing mighty deer!";
         } else {
-            rey.speed = 0;
+            rey.body.speed = 0;
             rey.stateText = "Time to bust a move! *begins break-dancing*";
         }
     }
+rey.basicUpdate();
 };
+
