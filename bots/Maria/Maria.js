6be3bc6dbe785aa5496f73e90e1127848aae5885
Maria Gasca <gascamaria1@icloud.com>
2016-02-16 12:37:53 -0800

    Maria Basic Update

--- bots/Maria/Maria.js
+++ bots/Maria/Maria.js
@@ -1,6 +1,4 @@
 var maria = new Bot(240, 220, 'maria', 'bots/maria/maria.png');
-maria.angle = 100; // Initial Angle
-maria.speed = 100; // Initial Speed
 
 maria.stateText = "The princess is here!";
 
@@ -8,18 +6,25 @@ maria.getStatus = function() {
     return maria.stateText;
 }
 
+maria.init = function() {
+    this.body = this.sprite.body;
+    maria.body.rotation = 100;
+    maria.body.speed = 100; 
+}
+
 maria.update = function() {
     if (Math.random() < .1) {
-        maria.angle += 10;
+        maria.incrementAngle(10);;
         maria.angle = maria.angle % 180;
     }
     if (Math.random() < .01) {
         if (Math.random() < .5) {
-            maria.speed = 100;
+            maria.body.speed = 100;
             maria.stateText = "Normal";
         } else {
-            maria.speed = 100;
+            maria.body.speed = 100;
             maria.stateText = "Back";
         }
     }
+    maria.basicupdate();
 };
