8f954aa9e0431c9370348592a3a0e2e3bf3ceca4
Daniel Hiebert <mennoknight55@gmail.com>
2016-02-16 12:48:05 -0800

    Updated my bot
    Espeon should be moving again.

--- bots/Daniel/Daniel.js
+++ bots/Daniel/Daniel.js
@@ -1,67 +1,81 @@
 var Daniel = new Bot(240, 220, 'Daniel', 'bots/Daniel/espeon.png');
-Daniel.angle = 100; // Initial Angle
-Daniel.speed = -70; // Initial Speed
-Daniel.confusion = 0;
+
+Daniel.init = function() {
+    this.body = this.sprite.body;
+    Daniel.body.angle = 100; // Initial Angle
+    Daniel.body.speed = -70; // Initial Speed
+    Daniel.confusion = 0;
+}
 
 
 Daniel.getStatus = function() {
-	return Daniel.stateText;
+    return Daniel.stateText;
+}
+
+Daniel.found = function() {
+    Daniel.confusion = 0;
 }
 
 Daniel.update = function() {
 
-	switch (Daniel.state) {
-		case 1: //doing fine
-			Daniel.speed = -70;
-			Daniel.stateText = "Everything is fine!";
-			if (Math.random() < .5) {
-        		Daniel.angle += 5;
-    		} 
-    		else {
-        		Daniel.angle -= 5;
-    		}
-    		Daniel.confusion++;
-    		break;
+    switch (Daniel.state) {
+        case 1: //doing fine
+            Daniel.body.speed = -70;
+            Daniel.stateText = "Everything is fine!";
+            if (Math.random() < .5) {
+                Daniel.incrementAngle(5);
+            } else {
+                Daniel.incrementAngle(-5);
+            }
+            Daniel.confusion++;
+            break;
 
-    	case 2: //getting lost
-    		Daniel.speed = -40;
-    		Daniel.stateText = "Getting lost.";
-    		if (Math.random() < .5) {
-        		Daniel.angle += 10;
-    		} 
-    		else {
-        		Daniel.angle -= 10;
-        	}
-        	Daniel.confusion++;
-        	break;
+        case 2: //getting lost
+            Daniel.body.speed = -40;
+            Daniel.stateText = "Getting lost.";
+            if (Math.random() < .5) {
+                Daniel.incrementAngle(10);
+            } else {
+                Daniel.incrementAngle(-10);
+            }
+            Daniel.confusion++;
+            break;
 
         case 3: //lost
-        	Daniel.speed = 0;
-        	Daniel.stateText = "Where am I?";
-        	if (Math.random() < .5) {
-        		Daniel.angle += 15;
-    		} 
-    		else {
-        		Daniel.angle -= 15;
-        	}
-        	Daniel.confusion--;
-        	break;
+            Daniel.body.speed = 0;
+            Daniel.stateText = "Where am I?";
+            if (Math.random() < .5) {
+                Daniel.incrementAngle(15);
+            } else {
+                Daniel.incrementAngle(-15);
+            }
+            Daniel.confusion--;
+            break;
+
+            // case 4: //found
+            //     Daniel.body.speed = 70;
+            //     Daniel.stateText = "Oh there I am!";
+            //     if (Math.random() < .5) {
+            //         Daniel.incrementAngle(5);
+            //     } else {
+            //         Daniel.incrementAngle(-5);
+            //     }
+            //     Daniel.confusion++;
+            //     break;
 
         default:
-        	Daniel.speed = 70;
-        	Daniel.stateText = "Something went wrong";
-        	break;
-	}
+            Daniel.body.speed = -70;
+            Daniel.stateText = "Something went wrong";
+            break;
+    }
 
-if (Daniel.confusion > 1000) {
-	Daniel.state = 3;
-}
-else if (Daniel.confusion > 500 && Daniel.confusion < 700) {
-	Daniel.state = 2;
-}
-else if (Daniel.confusion < 100) {
-	Daniel.state = 1;
-}
+    if (Daniel.confusion > 1000) {
+        Daniel.state = 3;
+    } else if (Daniel.confusion > 500 && Daniel.confusion < 700) {
+        Daniel.state = 2;
+    } else if (Daniel.confusion < 100) {
+        Daniel.state = 1;
+    }
 
+    Daniel.basicUpdate();
 };
-
