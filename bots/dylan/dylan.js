e7ec6eef66559625aede8d3fcc33a7a0cb192b15
dmyrie <dmyrie@ucmerced.edu>
2016-02-16 12:50:25 -0800

    More changes

--- bots/dylan/dylan.js
+++ bots/dylan/dylan.js
@@ -1,44 +1,39 @@
-var dylan = new Bot(240, 220, 'dylan', 'bots/dylan/player_car.png'); 
-    
-    dylan.angle = 90;
-    dylan.speed = 125;
-
-    dylan.stateText = "Status Normal: cruising speed";
+var dylan = new Bot(240, 220, 'dylan', 'bots/dylan/player_car.png');
 
 
 
 dylan.getStatus = function() {
-	return dylan.stateText;
+    return dylan.stateText;
 }
 
 //getStats & init for later??
-dylan.init= function(){ 
-this.body = this.sprite.body; 
-    dylan.body.rotation = 100; 
+dylan.init = function() {
+    this.body = this.sprite.body;
+    dylan.body.rotation = 100;
     dylan.body.speed = 100;
 }
 dylan.update = function() {
-	if (Math.random() <.1) {
-		dylan.incrementangle += 5;
-		dylan.incrementangle = dylan.incrementangle % 270;
+    if (Math.random() < .1) {
+        dylan.incrementangle += 5;
+        dylan.incrementangle = dylan.incrementangle % 270;
 
-	}
+    }
 
-	if (Math.random() < .03) {
-		if (Math.random() < .7) {
-			dylan.body.speed = 575;
-				dylan.stateText = "Punch it!";
-		
+    if (Math.random() < .03) {
+        if (Math.random() < .7) {
+            dylan.body.speed = 575;
+            dylan.stateText = "Punch it!";
 
-	
-	} else {
-		dylan.speed < 0;
-			dylan.stateText = "Reversing...";
-		dylan.speed == 0;
-			dylan.ststeText = "Collision!";	
 
-		}
 
-	}
+        } else {
+            dylan.speed == 0;
+            dylan.ststeText = "Collision!";
+
+        }
+
+    }
+
+    dylan.basicUpdate();
 
 };
