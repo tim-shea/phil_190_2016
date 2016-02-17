63cf69e8bd26984088d159404ace048775e279b4
Jeff Yoshimi <jyoshimi@ucmerced.edu>
2016-02-17 15:18:07 -0800

    Making myself the default default :)

--- preferences.js
+++ preferences.js
@@ -2,4 +2,4 @@
 //
 // GENERALLY SPEAKING, DO NOT CHECK CHANGES IN THIS FILE IN TO THE REPOSITORY!
 //
-var defaultBotIndex = 1; // Set to your bot index
+var defaultBotIndex = 0; // Set to your bot index
