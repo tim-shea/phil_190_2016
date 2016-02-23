65437f4b60911028984dc198f518cb626bc46c5c
reyflores94 <rsequeira@ucmerced.edu>
2016-02-23 00:27:59 -0800

    Update
    Included the state functions but instead of fatigue I included
    exhaustion

--- bots/rey/rey.js
+++ bots/rey/rey.js
@@ -1,28 +1,21 @@
 var rey = new Bot(240, 220, 'rey', 'bots/rey/whitedeer.png');
-// rey.body.angle = 100; // Initial Angle
-// rey.body.speed = 100; // Initial Speed
-// rey.body.rotation = 100; 
 
-
-rey.stateText = "Beware the break-dancing mighty deer!"; //break-dancing mighty deer because everytime it stops to rest it moves like crazy
-
+// (Override) Initialize Bot
 rey.init = function() {
-        this.body = this.sprite.body; // Todo:  a way to automate this?
-        rey.body.rotation = 100; // Initial Angle
-        rey.body.speed = 100; // Initial Speed
-        // Do something every n seconds.
+    this.body = this.sprite.body; // Todo:  a way to do this at a higher level?
+    this.body.rotation = 100; // Initial Angle
+    this.body.speed = 100; // Initial Speed
 
+    // Initialize Timed Updates
+    game.time.events.loop(Phaser.Timer.SECOND * 1, rey.update1Sec, this);
+    //game.time.events.loop(Phaser.Timer.SECOND * .01, rey.updateTenthSec, this);
+}
 
-        //game.time.events.loop(Phaser.Timer.SECOND * 1, rey.timedEvend, this);
-        //game.time.events.loop(Phaser.Timer.SECOND * 1, rey.update1Sec, this);
-        game.time.events.loop(Phaser.Timer.SECOND * 5, rey.updateFiveSec, this);
-
-    }
-    //
-    ///Motion Code
-    //
+//
+// Motion modes
+//
 rey.walking = {
-    description: "walking",
+    description: "Walking",
     update: function() {
         // Slight tilting when moving
         if (Math.random() < .5) {
@@ -31,78 +24,95 @@ rey.walking = {
         }
         // A leisurely place
         rey.body.speed = 200;
+        console.log("Someone join me!!!!");
+
     }
 }
 rey.still = {
-    description: "still",
+    description: "Still",
     update: function() {
         // Stand still
-        rey.body.speed = 100;
-        rey.body.rotation = 10;
+        rey.body.speed = 0;
+        console.log("Taking a break!");
+    }
+}
+rey.dancing = {
+    description: "Dancing",
+    update: function() {
+        // Wilder steering changes
+        if (Math.random() < .5) {
+            rey.incrementAngle(50 * Math.random() - 5);
+        }
+        // Fast
+        rey.body.speed = 1000;
+        console.log("Dun Dun Dun Dun");
     }
 }
 
-//
-//Emotion Code
-//
-// rey.angry = {
-//         name: "Angry",
-//         transitionProbability: .2,
-//         transition: function() {
-//             // Leave this state 80% of the time
-//             if (Math.random() < .8) {
-//                 // If exiting, go to back to calm
-//                 return rey.calm;
-//             } else {
-//                 return rey.angry;
-//             }
-//         },
-//         getMotionMode: function() {
-//             return rey.spazzing;
-//         }
 
-//     }
-    //
-    //current state
-    //
-rey.emotion = rey.calm;
+//
+// Emotion states
+//
+rey.energized = {
+    name: "energized",
+    transitionProbability: .5,
+    transition: function() {
+        if (Math.random() < .4) {
+            return rey.energized;
+        } else {
+            return rey.exhausted;
+        }
+    },
+    getMotionMode: function() {
+        if (Math.random() < .33) {
+            return rey.walking;
+        } else {
+            return rey.dancing;
+        }
+    }
+}
+rey.exhausted = {
+    name: "exhausted",
+    transitionProbability: .2,
+    transition: function() {
+        // Leave this state 80% of the time
+        if (Math.random() < .7) {
+            // If exiting, go to back to energized
+            return rey.energized;
+        } else {
+            return rey.exhausted;
+        }
+    },
+    getMotionMode: function() {
+        return rey.still;
+    }
+
+}
+
+// Current States
+rey.emotion = rey.energized;
 rey.motionMode = rey.walking;
 
-
+// (Override) Populate status field motionMode.description
 rey.getStatus = function() {
-    var statusString = jeff.emotion.name;
-    statusString += "\n-------";
-    statusString += "\nMotion mode: " + jeff.motionMode.description;
+    var statusString = rey.motionMode.description;
+    statusString += " because I am ";
+    statusString += rey.emotion.name;
+    statusString += ".";
     return statusString;
 }
 
 rey.update = function() {
     if (rey.atBoundary() === true) {
-        rey.incrementAngle(100);
+        rey.incrementAngle(45);
     }
     rey.motionMode.update(); // Todo: IncrementAngle does not work when called from timed functions.  Not sure why not.
     this.basicUpdate();
 };
 
-
-//rey.update = function() {
-// if (Math.random() < .1) {
-//     //rey.angle += 100;
-//     rey.incrementAngle(100);
-// }
-// if (Math.random() < .01) {
-//     if (Math.random() < .5) {
-//         rey.body.speed = 300;
-//         rey.stateText = "Here comes the break-dancing mighty deer!";
-//     } else {
-//         rey.body.speed = 0;
-//         rey.stateText = "Time to bust a move! *begins break-dancing*";
-//     }
-//}
-//     rey.basicUpdate();
-
-// }
-
-rey.updateFiveSec = function() {
-    console.log("Dancing")
+rey.update1Sec = function() {
+    if (Math.random() < rey.emotion.transitionProbability) {
+        rey.emotion = rey.emotion.transition();
+    }
+    rey.motionMode = rey.emotion.getMotionMode();
 }
