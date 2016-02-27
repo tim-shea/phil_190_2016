c33985db9960b31095060a3e7e034991c7e2559e
Jeff Yoshimi <jyoshimi@ucmerced.edu>
2016-02-27 14:41:47 -0800

    Minor cleanup to Daniel's random function

--- bots/bot.js
+++ bots/bot.js
@@ -54,11 +54,9 @@ Bot.prototype.atBoundary = function(amount) {
 }
 
 //
-// Helper function to update angle
-//
-// Todo: not totally clear why angle can't be used here. I have to use rotation.
-//
-
+// Returns a random number between floor and ceiling 
+// (Author: Daniel)
+// 
 Bot.prototype.getRandom = function(floor, ceiling) {
     var midpoint = (ceiling+floor)/2;
     var difference = ceiling-floor;
@@ -69,7 +67,6 @@ Bot.prototype.getRandom = function(floor, ceiling) {
 // Calculates midpoint and difference between the given amounts to calculate a random number between the amounts
 // Example of how to use:  "Daniel.incrementAngle(Daniel.getRandom(-5, 5));"
 //
-
 Bot.prototype.incrementAngle = function(amount) {
 	this.sprite.body.rotation += amount;
     this.sprite.body.rotation = this.sprite.body.rotation % 180;
