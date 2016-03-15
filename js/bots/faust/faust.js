94d7c9ee97ceaec7e7170a16e5be1841ec4fe47e
Faustina Barnard <fbarnard@ucmerced.edu>
2016-03-15 12:58:10 -0700

    Total bot refactor
    For ease of use, I have completely refactored my bot’s code to mirror
    jeff.js bot. I have kept my old code in a separate file to go back and
    work but for the time being my bot will be like this since this is the
    safest option. expect a completed and original code for faust.js by the
    end of spring break.

--- js/bots/faust/faust.js
+++ js/bots/faust/faust.js
@@ -1,285 +1,237 @@
-var faust = new Bot(300, 300, 'faust', 'js/bots/faust/faust.png');
+/**
+ * faust's bot
+ */
+var faust = new Bot(300, 300, 'faust', 'js/bots/faust/person.png');
 
-faust.speechText = "";
+/**
+ * State variables
+ */
+faust.currentlyPursuing = "Nothing";
+faust.currentMotion = Motions.still;
 
+/**
+ * Initialize bot
+ *
+ * @override
+ */
 faust.init = function() {
-    this.body = this.sprite.body;
+    this.body = this.sprite.body; // Todo:  a way to do this at a higher level?
     this.body.rotation = 100; // Initial Angle
     this.body.speed = 100; // Initial Speed
-    //faust.energy = 100 // Initial energy level
+
+    // Initialize Timed Updates
     game.time.events.loop(Phaser.Timer.SECOND * 1, faust.update1Sec, this);
-};
+    game.time.events.loop(Phaser.Timer.SECOND * 10, faust.updateTenSecs, this);
+    game.time.events.loop(Phaser.Timer.SECOND * 60 * 2, faust.update2min, this);
 
-faust.turn = function() {
-	faust.turnRandom = Math.random();
-	if (faust.turnRandom < .1) { // small left turn
-		faust.incrementAngle(5);
-	} else if (faust.turnRandom >= .1 && faust.turnRandom < .2) { // small right turn
-		faust.incrementAngle(-5)
-	}
-};
+    // Make productions.  Very dumb productions for now.
+    eatingProduction1 = new Production("eating", 
+        Production.priority.High, 
+        function() {return (faust.hunger.value > 10 && faust.hunger.value < 20 );},
+        function() {console.log("Eating 1");});
+    eatingProduction2 = new Production("eating", 
+        Production.priority.Low, 
+        function() {return (faust.hunger.value > 20 && faust.hunger.value < 30);},
+        function() {console.log("Eating 2");});
+    eatingProduction3 = new Production("eating", 
+        3, 
+        function() {return (faust.hunger.value > 30 && faust.hunger.value < 40);},
+        function() {console.log("Eating 3");});
 
-// Hunger 
-faust.hunger = {
-    amount: 0,
-    eat: function(food_amount) {
-        this.amount -= food_amount;
-        this.amount = Math.max(0,this.amount); // Don't allow hunger to go below 0
-    },
-    update: function() {
-        if(this.amount >= 100) {
-            // Do nothing.  Hunger is capped. 
-        } else {
-            this.amount++;
-        }
-    },
-    toString: function() {
-        var hungerLevel = "";
-        if (this.amount < 20) {
-            hungerLevel = "Not hungry";
-        } else if (this.amount < 60) {            
-            hungerLevel = "Hungry";
-        } else if (this.amount < 80) {            
-            hungerLevel = "Starving!!";
-        } else {            
-            hungerLevel = "FEED ME!";
-        }
-        return hungerLevel + " (Hunger = " + this.amount + ")";
+    // Populate production list
+    this.productions = [eatingProduction1, eatingProduction2, eatingProduction3];
+}
+
+
+
+/**
+ * Markov process controlling emotions
+ */
+faust.emotions = new MarkovProcess("Calm");
+faust.emotions.add("Calm", [
+    ["Calm", "Happy", "Angry", "Sad"],
+    [.8, .1, .05, .05]
+]);
+faust.emotions.add("Sad", [
+    ["Sad", "Calm"],
+    [.7, .3]
+]);
+faust.emotions.add("Angry", [
+    ["Angry", "Calm"],
+    [.5, .5]
+]);
+faust.emotions.add("Happy", [
+    ["Happy", "Calm"],
+    [.7, .3]
+]);
+
+/**
+ * Hunger Variable
+ */
+faust.hunger = new DecayVariable(0, 1, 0, 100);
+faust.hunger.toString = function() {
+    var hungerLevel = "";
+    if (this.value < 20) {
+        hungerLevel = "Not hungry";
+    } else if (this.value < 60) {
+        hungerLevel = "Hungry";
+    } else if (this.value < 80) {
+        hungerLevel = "Starving!!";
+    } else {
+        hungerLevel = "FEED ME!";
     }
-};
+    return hungerLevel + " (Hunger = " + this.value + ")";
+}
 
-
-//
-//Stats
-//
-
-/*faust.energy = {
-	amount: 100,
-	max: 150,
-	min: 0,
-	rest: function(need_energy) {
-		this.amount = need_energy;
-		this.amount = Math.min(0, this.amount); //max energy is 100 and can't go below 0
-	},
-	update: function() {
-		if(this.amount = 100) {
-			// fine. energy is capped.
-		} else {
-			this.amount--;
-		}
-	},
-	toString: function() {
-		var energyLevel = "";
-		if (this.amount >= 70) {
-			energyLevel = "fit as a fiddle";	
-		} else if (this.amount < 70) {
-			enrgyLevel = "losing energy";
-		} else if (this.amount < 30) {
-			energyLevel = "Running low on energy!!";
-		} else if (this.amount < 10) {
-			energyLevel = "NEED REST!!";
-		} else if (this.amount = 0) {
-			faust.energy = faust.still
-		}
-		return enrgyLevel + " (Energy = " + this.amount + ")";
-	},
-}*/
-
-//
-// Motion states
-//
-
-faust.walk = {
-    description: "walking",
-    update: function() {
-    	if (Math.random() < .5) {
-            faust.incrementAngle(10 * Math.random() - 5);
-        }
-        faust.body.speed = 100;
-    },
-    //adjustNeeds: function() {
-    //	faust.energy.amount++;
-    //}
-};
-
-faust.run = {
-	description: "running!",
-	update: function() {
-		if (Math.random() < .5) {
-			faust.incrementAngle(10 * Math.random() - 5);
-		}
-		faust.body.speed = 200;
-	},
-	//adjustNeeds: function() {
-	//	faust.energy.amount -=5;
-	//}
-};
-
-faust.sonicSpeed = {
-	description: "SONIC SPEED!",
-	update: function() {
-		if (Math.random() < .5) {
-			faust.incrementAngle(10 * Math.random() - 5);
-		}
-		faust.body.speed = 500;
-	},
-	//adjustNeeds: function() {
-	//	faust.energy.amount -=10
-	//}
-};
-
-faust.still = {
-    description: "still",
-    update: function() {
-        faust.body.speed = 0;
-    },
-    //adjustNeeds: function() {
-    //	faust.energy.amount += 10;
-    //}
-};
-
-faust.moping = {
-    description: "moping",
-    update: function() {
-        if (Math.random() < .05) {
-            faust.incrementAngle(10 * Math.random() - 5);
-        }
-        faust.body.speed = 50;
-    }
-};
-
-//
-// Emotion States
-//
-
-faust.calm = {
-    name: "Calm",
-    transitionProbability: .05,
-    transition: function() {
-        if (Math.random() < .8) {
-            return faust.happy;
-        } else {
-            return faust.angry;
-        }
-    },
-    getMotionState: function() {
-        if (Math.random() < .4) {
-            return faust.still;
-        } else {
-            return faust.moping;
-        }
-    }
-};
-
-faust.angry = {
-    name: "Angry",
-    transitionProbability: .2,
-    transition: function() {
-        // Leave this state 80% of the time
-        if (Math.random() < .8) {
-            // If exiting, go to back to calm
-            return faust.calm;
-        } else {
-            return faust.angry;
-        }
-    },
-    getMotionState: function() {
-        return faust.sonicSpeed;
-    }
-
-};
-
-faust.happy = {
-    name: "Happy",
-    transitionProbability: .01,
-    transition: function() {
-        if (Math.random() < .8) {
-            return faust.calm;
-        } else {
-            return faust.angry;
-        }
-    },
-    getMotionState: function() {
-        // When happy, either walk or spaz
-        if (Math.random() < .8) {
-            return faust.run;
-        } else {
-            return faust.sonicSpeed;
-        }
-    }
-};
-
-faust.sad = {
-    name: "Sad",
-    transitionProbability: .3,
-    transition: function() {
-        if (Math.random() < .8) {
-            return faust.calm;
-        } else {
-            return faust.angry;
-        }
-    },
-    getMotionState: function() {
-        return faust.moping;
-    }
-};
-
-//
-// Other Stuff
-//
-
-// Current States
-faust.motionState = faust.walk;
-faust.emotionState = faust.calm; 
-
-// (Override) Populate status field
+/**
+ * Populate the status field
+ *
+ * @override
+ */
 faust.getStatus = function() {
-    var statusString = "Emotion: " + faust.emotionState.name;
-    statusString += "\nMotion: " + faust.motionState.description;
+    var statusString = "Emotion: " + faust.emotions.current;
+    statusString += "\nMotion: " + faust.currentMotion.description;
     statusString += "\n" + faust.hunger.toString();
-    statusString += "\nSpeech: " + faust.speechText;
+    // statusString += "\nEntity collisions: " +
+    //     faust.getOverlappingEntities().map(function(item) {
+    //         return item.name;
+    //     });
+    // statusString += "\nBot collisions: " +
+    //     faust.getOverlappingBots().map(function(item) {
+    //         return item.name;
+    //     });
+    statusString += "\nMoving to: " + faust.currentlyPursuing;
     return statusString;
-};
+}
 
+/**
+ * Set the current motion state.  Currently updated every second.
+ */
+faust.setMotion = function() {
+
+    // Default markov chain movement patterns
+    // TODO: Add conditions that involve hunger, etc.
+    if (faust.emotions.current === "Sad") {
+        faust.currentMotion = Motions.moping;
+    } else if (faust.emotions.current === "Happy") {
+        faust.currentMotion = Motions.walking;
+    } else if (faust.emotions.current === "Calm") {
+        let rnd = Math.random();
+        if (rnd < .5) {
+            faust.currentMotion = Motions.still;
+        } else {
+            faust.currentMotion = Motions.walking;
+        }
+    } else if (faust.emotions.current === "Angry") {
+        faust.currentMotion = Motions.spazzing;
+    }
+}
+
+/**
+ * When a pursuit is completed reset the pursuit string.
+ */
+faust.pursuitCompleted = function() {
+    faust.currentlyPursuing = "Nothing";
+    this.currentMotion = Motions.still;
+}
+
+//////////////////////
+// Update Functions //
+//////////////////////
+
+/**
+ * Main update called by the phaer game object (about 40 times / sec. on my machine).
+ *
+ * @override
+ */
 faust.update = function() {
-    if (faust.atBoundary() === true) {
-        faust.incrementAngle(45);
-    };
-    faust.motionState.update(); 
-    this.basicUpdate();
-    faust.genericUpdate(); //"Superclass" update method, can override the collision status
+
+    // Apply current motion
+    this.currentMotion.apply(faust);
+    // "Superclass" update method
+    faust.genericUpdate();
 };
 
+
+/**
+ * Called every second
+ */
+faust.update1Sec = function() {
+    faust.hunger.increment();
+    faust.emotions.update();
+    faust.setMotion();
+    fireProductions(faust.productions);
+
+}
+
+/**
+ * Called every ten seconds
+ */
+faust.updateTenSecs = function() {
+    // Pursue a random entity
+    if (Math.random() < .9) {
+        // faust.currentlyPursuing = this.pursueRandomObject(2000).name;
+    }
+}
+
+/**
+ *  Called every two minutes
+ */
+faust.update2min = function() {
+    // faust.hunger.setValue(0);
+}
+
+///////////////////////////
+// Interaction Functions //
+///////////////////////////
+
+
+/**
+ * React to a collision.
+ *
+ * @override
+ */
 faust.collision = function(object) {
     // console.log("Object is edible: " + object.isEdible);
-    if (!faust.speechText.contains(object.name)) {
-        faust.speechText += " Hello " + object.name + ".";
+    if (object.isEdible) {
+        faust.eatObject(object);
+    } else {
+        faust.speak(object, "Hello " + object.name);
     }
-    faust.speak(object, "Hello " + object.name);
+    // I'm a nice guy
+    if (object instanceof Bot) {
+        object.highFive();
+    }
+
     // faust.flee(object);
     // faust.pursue(object);
-};
+ }
 
-//speak vs hear -----> speak is bot and hear is the override of speak in my specific bot
+/**
+ * Call this when eating something.  
+ *
+ * @param {Entity} objectToEat what to eat
+ */
+faust.eatObject = function(objectToEat) {
+    objectToEat.eat();
+    faust.hunger.subtract(objectToEat.calories);
+    faust.speak(objectToEat, "Yummy " + objectToEat.description + "!");
+}
+
+/**
+ * Reaction to hearing something.
+ *
+ * @override
+ */
 faust.hear = function(botWhoSpokeToMe, whatTheySaid) {
-    if (!faust.speechText.contains("Oh you")) {
-        faust.speechText += " Oh you just said " + whatTheySaid + ".";
-    }
-};
+    faust.speak(botWhoSpokeToMe, "Right on " + botWhoSpokeToMe.name); // TODO: Make more intelligent responses!
+}
 
-faust.update1Sec = function() {
-	faust.hunger.update();
-    if (Math.random() < faust.emotionState.transitionProbability) {
-        faust.emotionState = faust.emotionState.transition();
-    };
-    faust.motionState = faust.emotionState.getMotionState();
-    faust.hunger.eat(101);
-    faust.speechText = "";
-};
-
-
-
-
-
-
-
+/**
+ * React when someone high fives me.
+ *
+ * @override
+ */
+faust.highFived = function(botWhoHighFivedMe) {
+    faust.speak(botWhoHighFivedMe, "Hey what's up " + botWhoHighFivedMe.name + ".");
+}
